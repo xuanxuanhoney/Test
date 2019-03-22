1384f813415843b9a4f93705ebb1f0d1bc13cdc2
xuanxuanhoney <1041308160@qq.com>
2019-03-22 18:54:11 +0800

    ADD functions(add,sub,mul,div)

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,4 +7,19 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		return a/b;
+	}
 }
