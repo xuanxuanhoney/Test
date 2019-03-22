30fdc810388991b7bcacd585770201838bd9e418
wangqn <wangqn0718@gmail.com>
2019-03-22 14:44:49 +0100

    test ABS

--- src/test/java/calc/CaculatorTest.java
+++ src/test/java/calc/CaculatorTest.java
@@ -39,4 +39,8 @@ public class CaculatorTest {
 	public void testPresedence() {
 		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
 	}
+	@Test
+	public void testAbs(){
+		assertEquals(6, calc.abs(-6));
+	}
 }
