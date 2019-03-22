4689e7c0c494c404109b0092822c9782b177c382
xuanxuanhoney <1041308160@qq.com>
2019-03-22 19:01:23 +0800

    1901

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -8,18 +8,18 @@ public class CalculatorMain {
 		return a+b;
 	}
 	
-	public int sub(int a, int b) {
-		return a-b;
-	}
-	
-	public int mul(int a, int b) {
-		return a*b;
-	}
-	
-	public int div(int a, int b) {
-		if(b==0) {
-			return 0;
-		}
-		return a/b;
-	}
+//	public int sub(int a, int b) {
+//		return a-b;
+//	}
+//	
+//	public int mul(int a, int b) {
+//		return a*b;
+//	}
+//	
+//	public int div(int a, int b) {
+//		if(b==0) {
+//			return 0;
+//		}
+//		return a/b;
+//	}
 }
