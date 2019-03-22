118bdd984e53ce29f1b189a13d1f53f540b57e03
xuanxuanhoney <1041308160@qq.com>
2019-03-22 19:09:24 +0800

    1909test add functions

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,23 +7,20 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
-<<<<<<< .merge_file_a04708
 	
-//	public int sub(int a, int b) {
-//		return a-b;
-//	}
-//	
-//	public int mul(int a, int b) {
-//		return a*b;
-//	}
-//	
-//	public int div(int a, int b) {
-//		if(b==0) {
-//			return 0;
-//		}
-//		return a/b;
-//	}
-=======
-	// bth tu edits
->>>>>>> .merge_file_a15736
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		if(b==0) {
+			return 0;
+		}
+		return a/b;
+	}
+
 }
